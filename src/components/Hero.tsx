348a4555fc9ce1008d82f1f3f8d7600d33b12b47
Helder M. V. R. <manuhelderruiz@gmail.com>
2025-06-04 11:16:47 +0200

    feat(Hero.tsx): hero layout ready, responsive done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,35 @@
- import { PrincipalTitle } from "./ui";
- import ButtonIcon from "./ui/ButtonIcon";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { PrincipalTitle } from "./ui";
+import ButtonIcon from "./ui/ButtonIcon";
 import PrincipalLogo from "./ui/PrincipalLogo";
- import QuoteCard from "./ui/QuoteCard";
- import SocialNav from "./ui/SocialNav";
+import QuoteCard from "./ui/QuoteCard";
+import SocialNav from "./ui/SocialNav";
 
 const Hero = () => {
+  const isSmallScreen = useMediaQuery("(max-width: 550px)");
+
   return (
     <>
       <header className="relative h-dvh w-full flex flex-col justify-around items-center">
-        <PrincipalLogo />
-        <PrincipalTitle
-          name="HELDER"
-          lastName="RUIZ"
-          roleLine1="FullStack"
-          roleLine2="Developer"
-        />
+        {isSmallScreen ? (
+          <PrincipalLogo />
+        ) : (
+          <PrincipalTitle
+            name="HELDER"
+            lastName="RUIZ"
+            roleLine1="FullStack"
+            roleLine2="Developer"
+          />
+        )}
 
         <QuoteCard />
 
         <SocialNav />
 
-        <ButtonIcon idName="icon-chevrons-down" className="text-neutral-400/20 animate-bounce" />
+        <ButtonIcon
+          idName="icon-chevrons-down"
+          className="text-neutral-400/20 animate-bounce"
+        />
       </header>
     </>
   );
