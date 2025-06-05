842787d4e65c85373f06c825f1219863ce331cce
Helder M. V. R. <manuhelderruiz@gmail.com>
2025-06-05 12:37:34 +0200

    feat: MyInspirationCard done

--- src/components/ui/MyInspirationCard.tsx
+++ src/components/ui/MyInspirationCard.tsx
@@ -1,34 +1,49 @@
+import { useEffect, useState } from "react";
+import { books, music, people, podcasts } from "../../data/inspirations";
+import type { Inspiration } from "../../types/inspiration";
+
 const MyInspirationalCard = () => {
+  const [inspiration, setInspiration] = useState<Inspiration | null>(null);
+
+  useEffect(() => {
+    const allInspirations: Inspiration[] = [
+      ...books,
+      ...music,
+      ...people,
+      ...podcasts,
+    ];
+
+    const randomIndex = Math.floor(Math.random() * allInspirations.length);
+    const randomInspiration = allInspirations[randomIndex];
+
+    setInspiration(randomInspiration);
+  }, []);
+
+  if (!inspiration) return null;
+
   return (
-    /* personal inspiration card -> create local "data base". 
-       people|books|podcast|music. */
     <div
       id="my-inspiration"
       className="flex flex-col gap-4 w-xs h-auto text-xs border border-indigo-500 bg-indigo-800/10 shadow-[0_0_5px_#6366f1] rounded-2xl p-4"
     >
-      <h3>
-        <span id="my-inspiration-type" className="text-indigo-500 font-bold">
-          PEOPLE
+      <h3 className="flex items-center">
+        <span id="my-inspiration-type" className="text-indigo-500 font-bold text-base ">
+          {inspiration.type}
         </span>
         | that inspires me
       </h3>
-      <div className="flex gap-4 border-l-1 border-l-indigo-500 pl-4">
+      <div className="flex items-center gap-4 border-l-1 border-l-indigo-500 pl-4">
         <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/0_S%C3%A9n%C3%A8que_-_Mus%C3%A9e_du_Prado_-_Cat._144_-_%282%29.JPG/250px-0_S%C3%A9n%C3%A8que_-_Mus%C3%A9e_du_Prado_-_Cat._144_-_%282%29.JPG"
-          alt="Imagen de Séneca"
+          src={inspiration.imageURL}
+          alt={`Imagen de ${inspiration.name}`}
           className="w-14 h-14 object-cover aspect-square border border-amber-400 shadow-[0_0_5px_#FFB900] rounded-2xl"
         />
 
         <div className="flex flex-col gap-2">
-          <h4 id="my-inspiration-name" className="text-amber-400 text-base">
-            Séneca
+          <h4 id="my-inspiration-name" className="text-amber-400">
+            {inspiration.name}
           </h4>
-          <p id="my-inspiration-desc">
-            Era un men muy listo que nació en un sitio de la antigüedad y daba
-            la chapa, pero no chapa de aburrir, chapa de "buah, como le sabe
-            este tío. Menudos factos suelta". Y al parecer motivó a la peña. No
-            veas con el cordobés.
-          </p>
+          <p id="my-inspiration-desc" className="text-balance">{inspiration.description}</p>
         </div>
       </div>
     </div>
