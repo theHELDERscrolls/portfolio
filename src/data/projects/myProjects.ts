262f78d8fa170c2fdd711147948d43c91eb38bef
Helder M. V. R. <manuhelderruiz@gmail.com>
2025-06-13 16:05:15 +0200

    refactor: meta tags done. Get ready for deploy

--- src/data/projects/myProjects.ts
+++ src/data/projects/myProjects.ts
@@ -2,10 +2,20 @@ import type { ProjectProps } from "../../types";
 
 export const myProjects: ProjectProps[] = [
   {
+    imgUrl: "src/assets/projects/project_heracles.jpg",
+    title: "Heracles",
+    description:
+      "When I look at the night sky and feel how vast the universe is, I don't feel small. I feel great, because my atoms come from those very stars. Being alive and conscious is the universe's way of knowing itself.",
+    tech: ["Unknown"],
+    githubURL: "",
+    deployURL: "",
+    status: "coming-soon",
+  },
+  {
     imgUrl: "src/assets/projects/project_bs.png",
     title: "Brawl Stats",
     description:
-      "A web to explore Brawl Stars characters and view their stats across different maps.",
+      "Check key stats and data for Brawl Stars with this fan-made tool. Explore brawlers’ performance, compare metrics, and access intuitive visualizations. Perfect for players and enthusiasts looking to improve their strategy.",
     tech: ["Rest API", "JavaScript", "CSS", "HTML"],
     githubURL: "https://github.com/theHELDERscrolls/Brawl_Stats",
     deployURL: "https://brawl-stats-fan-made.vercel.app/",
@@ -21,14 +31,9 @@ export const myProjects: ProjectProps[] = [
     deployURL: "https://skyrimzon.vercel.app/",
     status: "completed",
   },
-  {
-    imgUrl: "src/assets/projects/project_heracles.jpg",
-    title: "Heracles",
-    description:
-      "A new project is launching soon. Prepare for an interstellar journey as we build something out of this world.",
-    tech: ["Unknown"],
-    githubURL: "",
-    deployURL: "",
-    status: "coming-soon",
-  },
 ];
+
+/* 
+  Coming soon default phrase:
+  "We live on an insignificant planet of a humdrum star lost in a galaxy tucked away in some forgotten corner of a universe in which there are far more galaxies than people. And yet, we have dared to contemplate the origin of time, to explore the nature of matter, and to question our place in the cosmos"
+*/
