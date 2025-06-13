19881bf3c558e9cc1294bf713aa763d6eb6b5a9c
Helder M. V. R. <manuhelderruiz@gmail.com>
2025-06-13 16:37:45 +0200

    refactor: navbar style changed. Now the icons are bigger

--- src/components/ui/nav/Navbar.tsx
+++ src/components/ui/nav/Navbar.tsx
@@ -13,12 +13,12 @@ const navItems = [
 const Navbar = ({ activeSection }: NavbarProps) => {
   return (
     <nav className="sticky z-50 flex items-center justify-center w-full top-2 bottom-1 sm:top-4 ">
-      <ul className="flex gap-2 p-2 rounded-full shadow-lg sm:gap-4 sm:p-4 bg-indigo-900/10 backdrop-blur-md sm:w-fit">
+      <ul className="flex gap-8 px-4 py-2 rounded-full shadow-lg sm:gap-4 sm:p-4 bg-indigo-900/10 backdrop-blur-md w-fit">
         {navItems.map((item) => (
           <li key={item.id}>
             <a href={item.href}>
               <svg
-                className={`w-4 h-4 sm:w-8 sm:h-8 transition-all duration-300 ${
+                className={`w-6 h-6 sm:w-8 sm:h-8 transition-all duration-300 ${
                   activeSection === item.id
                     ? "text-indigo-400 scale-110 drop-shadow-[0_0_5px_#6366f1]"
                     : "text-neutral-400 hover:text-indigo-500"
