604b64a4205bb3fc79f1ac03c73e7ff7080505e2
Helder Manuel Vieira Ruiz <manuhelderruiz@gmail.com>
2026-06-05 23:36:11 +0200

    chore: imports warnings fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import path, { dirname } from "path";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import path, { dirname } from "node:path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
